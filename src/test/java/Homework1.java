efa79b3138d72c834b74ef2c2fb5f1ef779224a0
Karina Brikina <karina.brikina1989@gmail.com>
2021-04-28 22:53:57 +0300

    First homework

--- src/test/java/Homework1.java
+++ src/test/java/Homework1.java
@@ -9,12 +9,6 @@ import org.w3c.dom.css.CSSStyleDeclaration;
 import java.util.List;
 
 public class Homework1 {
-    private final By LOCATOR_BY_ID = By.id("elementID");
-    private final By LOCATOR_BY_NAME = By.name("elementName");
-    private final By LOCATOR_BY_TAGNAME = By.tagName("nameOfTag");
-    private final By LOCATOR_BY_CLASS = By.className("elementClass");
-    private final By LOCATOR_BY_XPATH = By.xpath(".//*[contains(@class, 'list-article__meta-info flex')]");
-
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode = 'primary']");
     private final By FIND_ARTICLE = By.tagName("article");
     private final By FIND_A_TAG = By.tagName("a");
@@ -25,38 +19,6 @@ public class Homework1 {
     private final By FIND_TOP_MENU = By.className("menu-items--top");
     private final By FIND_RUS_BTN_LINK = By.xpath(".//a[@href = 'https://rus.tvnet.lv/']");
 
-
-//    @Test
-//    public void firstArticleComments() {
-//        //WebDriver = browser window
-//        System.setProperty("webdriver.chrome.driver", "/users/karina/Documents/chromedriver");
-//        WebDriver browserWindow = new ChromeDriver();
-//        browserWindow.manage().window().maximize();
-//        browserWindow.get("http://tvnet.lv");
-//
-//        browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
-//        WebElement firstArticle = browserWindow.findElement(FIND_FIRST_ARTICLE);
-//        firstArticle.findElement(FIND_A_TAG).click();
-//        browserWindow.findElement(FIND_COMMENTS_BTN).click();
-//    }
-//
-//    @Test
-//    public void printFirstArticleTitle() {
-//        //WebDriver = browser window
-//        System.setProperty("webdriver.chrome.driver", "/users/karina/Documents/chromedriver");
-//        WebDriver browserWindow = new ChromeDriver();
-//        browserWindow.manage().window().maximize();
-//        browserWindow.get("http://tvnet.lv");
-//
-//        browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
-//        WebElement articleTitle = browserWindow.findElement(FIND_ARTICLE_TITLE);
-//
-//        System.out.println(articleTitle.getText());
-//
-//    }
-
-
-    //////////
 
     public ChromeDriver browserWindow;
 
