ec86fb7641ecd8c68d775dd152618d6abdac670d
Karina Brikina <karina.brikina1989@gmail.com>
2021-05-26 01:46:53 +0300

    Added LOGGER.info to DelfiArticleCommentsTest and Homework1

--- src/test/java/DelfiArticleCommentsTest.java
+++ src/test/java/DelfiArticleCommentsTest.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DelfiArticleCommentsTest {
@@ -19,36 +24,72 @@ public class DelfiArticleCommentsTest {
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-md-30')]");
     private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'text-size-md-28')]");
 
+    private final Logger LOGGER = LogManager.getLogger(DelfiArticleCommentsTest.class);
+
+    private final By ARTICLE_COMMENTS = By.className("type-cnt");
+
     private WebDriver driver;
 
     @Test
     public void titleAndCommentsCountCheck() {
+        LOGGER.info("This test is checking titles and comments count on home/article/comments pages");
+
+        LOGGER.info("Setting driver location");
         System.setProperty("webdriver.chrome.driver", "/users/karina/Documents/chromedriver");
+
+        LOGGER.info("Opening browser window");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        LOGGER.info("Opening Home page");
         driver.get("http://delfi.lv");
 
-        //-----------------------HOME PAGE----------------------------------
+        //--------------------HOME PAGE----------------------------------
         WebDriverWait wait = new WebDriverWait(driver, 10);
+        LOGGER.info("Waiting for accept cookies btn");
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIE_BTN));
 
+        LOGGER.info("Accepting cookies");
         driver.findElement(ACCEPT_COOKIE_BTN).click();
 
         List<WebElement> articles = driver.findElements(HOME_PAGE_ARTICLE);
-        WebElement article = articles.get(4);
+        WebElement article = articles.get(3);
 
+        LOGGER.info("Getting article title and comments count");
         String homePageTitle = article.findElement(HOME_PAGE_TITLE).getText();
         int homePageCommentsCount = getCommentsCount(article, HOME_PAGE_COMMENTS);
+        LOGGER.info("Title is: " + homePageTitle + " and comments count is: " + homePageCommentsCount);
 
+        LOGGER.info("Opening article page");
         article.findElement(HOME_PAGE_TITLE).click();
 
-        //-------------------ARTICLE PAGE--------------------
+        //-------------------ARTICLE PAGE-----------------------------------
+        LOGGER.info("Getting article title and comments count");
         String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
         int articlePageCommentsCount = getCommentsCount(ARTICLE_PAGE_COMMENTS);
+        LOGGER.info("Title is: " + articlePageTitle + " and comments count is: " + articlePageCommentsCount);
 
+        LOGGER.warn("Checking equality of homepage title and article title");
         Assertions.assertEquals(homePageTitle, articlePageTitle, "Wrong title!");
+        LOGGER.warn("Checking equality of homepage title's comments count and article title's comment count");
         Assertions.assertEquals(homePageCommentsCount, articlePageCommentsCount, "Wrong comments count!");
+
+        LOGGER.info("Opening article comments page");
+        driver.findElement(ARTICLE_PAGE_COMMENTS).click();
+
+        //--------------------COMMENTS PAGE-------------------------------
+        LOGGER.info("Getting article comments count summary");
+        List<WebElement> articleComments = driver.findElements(ARTICLE_COMMENTS);
+        int allCommentsCount = 0;
+
+        for (WebElement comments : articleComments) {
+            int commentsQty = removeBrackets(comments);
+            allCommentsCount += commentsQty;
+        }
+        LOGGER.info("Article comments summary Anonymous + Registered: " + allCommentsCount);
+
+        LOGGER.warn("Checking equality of home page title's comment count and article's comments count summary");
+        Assertions.assertEquals(homePageCommentsCount, allCommentsCount, "Wrong comments count in article!");
     }
 
     private int getCommentsCount(By locator) {
@@ -61,7 +102,7 @@ public class DelfiArticleCommentsTest {
         return commentsCount;
     }
 
-    private  int getCommentsCount(WebElement we, By locator) {
+    private int getCommentsCount(WebElement we, By locator) {
         int commentsCount = 0;
 
         if (!we.findElements(locator).isEmpty()) {
@@ -79,6 +120,7 @@ public class DelfiArticleCommentsTest {
 
     @AfterEach
     public void closeBrowser() {
+        LOGGER.info("Closing browser window");
         driver.close();
     }
 }
--- src/test/java/Homework1.java
+++ src/test/java/Homework1.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,51 +25,63 @@ public class Homework1 {
     private final By FIND_RUS_BTN_LINK = By.xpath(".//a[@href = 'https://rus.tvnet.lv/']");
     private final By FIND_ARTICLE_COMMENTS = By.className("list-article__comment");
 
+    private final Logger LOGGER = LogManager.getLogger(Homework1.class);
 
     private ChromeDriver driver;
 
     @BeforeEach
     public void setUP() {
-        //WebDriver = browser window
+        LOGGER.info("Setting driver location");
         System.setProperty("webdriver.chrome.driver", "/users/karina/Documents/chromedriver");
+
+        LOGGER.info("Opening browser window");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        LOGGER.info("Opening Home page");
         driver.get("http://tvnet.lv");
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
+        LOGGER.info("Waiting for accept cookies btn");
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
 
+        LOGGER.info("Accepting cookies");
         driver.findElement(ACCEPT_COOKIES_BTN).click();
-    }
-
-    @AfterEach
-    public void finished() {
-        driver.close();
     }
 
     // 1. Перейти на http://tvnet.lv, открыть первую статью и перейти на страницу комментариев.
     @Test
     public void firstArticleComments() {
+        LOGGER.info("This test opening first article page and comments page");
         WebElement firstArticle = driver.findElement(FIND_ARTICLE);
+        LOGGER.info("Opening first article page");
         firstArticle.findElement(FIND_A_TAG).click();
 
-        //Sometimes no comments(throw error)
+        LOGGER.info("Opening article comments page");
         driver.findElement(FIND_COMMENTS_BTN).click();
     }
 
     // 2. Перейти на http://tvnet.lv и распечатать в консоль заголовок первой статьи.
     @Test
     public void printFirstArticleTitle() {
+        LOGGER.info("This test getting first article's title text");
         WebElement articleTitle = driver.findElement(FIND_ARTICLE_TITLE);
+        String articleTitleText = articleTitle.getText();
+        boolean withComments = !articleTitle.findElements(FIND_ARTICLE_COMMENTS).isEmpty();
 
-        System.out.println(articleTitle.getText());
+        if (withComments) {
+            String commentsBtnText = articleTitle.findElement(FIND_ARTICLE_COMMENTS).getText();
+            articleTitleText = articleTitle.getText().replace(commentsBtnText, "");
+        }
 
+        LOGGER.info("First article title is: " + articleTitleText);
     }
 
     // 3. Создать и расписать локаторы для :
     // 3.1. Всех заголовков статей
     @Test
     public void articleTitles() {
+        LOGGER.info("Finding locator for all article tittles ");
         List<WebElement> allArticles = driver.findElements(FIND_ARTICLE_TITLE);
 
         System.out.println(allArticles);
@@ -76,6 +90,7 @@ public class Homework1 {
     // 3.2. Всех элементов с количеством комментариев.
     @Test
     public void getElementsWithComments() {
+        LOGGER.info("Finding locator for all elements with comments");
         List<WebElement> elementsWithComments = driver.findElements(FIND_ELEMENT_WITH_COMMENTS);
 
         System.out.println(elementsWithComments);
@@ -84,37 +99,41 @@ public class Homework1 {
     // 3.3. Для логотипа tvnet.lv
     @Test
     public void getLogo() {
+        LOGGER.info("Finding locator for http://tvnet.lv logo");
         WebElement tvnetLogo = driver.findElement(FIND_TVNET_LOGO);
-
-        System.out.println(tvnetLogo);
+        LOGGER.info("Logo locator is: " + tvnetLogo);
     }
 
     // 3.4. Для ссылки переключения на русский язык (RUS).
     @Test
     public void switchRusBtnLink() {
+        LOGGER.info("This test finding locator for RUS button link and change page language to russian");
+        LOGGER.info("Finding RUS button link");
         WebElement topMenu = driver.findElement(FIND_TOP_MENU);
         WebElement rusBtnLink = topMenu.findElement(FIND_RUS_BTN_LINK);
+
+        LOGGER.info("Change page language to russian");
         rusBtnLink.click();
-
-//        System.out.println(rusBtnLink.getText());
-
     }
 
     // 4. Распечатать в консоль заголовки всех статей на главной странице.
     @Test
     public void printArticleTitles() {
+        LOGGER.info("This test finds all main page articles and prints their titles");
+        LOGGER.info("Getting all main page articles title list");
         List<WebElement> allArticles = driver.findElements(FIND_ARTICLE);
         for (int i = 0; i < allArticles.size(); i++) {
             WebElement article = allArticles.get(i);
             String title = article.findElement(FIND_ARTICLE_TITLE).getText();
             boolean withComments = !article.findElements(FIND_ARTICLE_COMMENTS).isEmpty();
+            int articleCounter = i + 1;
 
             if (withComments) {
                 String commentsBtnText = article.findElement(FIND_ARTICLE_COMMENTS).getText();
                 title = title.replace(commentsBtnText, "");
             }
 
-            System.out.println(title);
+            LOGGER.info("Article title " + articleCounter + ": " + title);
         }
     }
 
@@ -122,12 +141,17 @@ public class Homework1 {
     // но и рядом отдельно проставить колличество комментариев.
     @Test
     public void printArticleInfo() {
+        LOGGER.info("This test finds all main page articles, prints their titles and comments count");
+        LOGGER.info("Getting all main page articles title list and comments count");
         List<WebElement> allArticles = driver.findElements(FIND_ARTICLE);
-        for (WebElement article : allArticles) {
+//        for (WebElement article : allArticles) {
+        for (int i = 0; i < allArticles.size(); i++) {
+            WebElement article = allArticles.get(i);
             WebElement title = article.findElement(FIND_ARTICLE_TITLE);
             boolean withComments = !article.findElements(FIND_ARTICLE_COMMENTS).isEmpty();
             String commentsBtnText;
             String finalResult;
+            int articleCounter = i + 1;
 
             if (withComments) {
                 commentsBtnText = article.findElement(FIND_ARTICLE_COMMENTS).getText();
@@ -137,8 +161,13 @@ public class Homework1 {
                 finalResult = title.getText() + " : 0";
             }
 
-            System.out.println(finalResult);
+            LOGGER.info("Article title " + articleCounter + ": " + finalResult);
         }
     }
 
+    @AfterEach
+    public void finished() {
+        LOGGER.info("Closing browser window");
+        driver.close();
+    }
 }
